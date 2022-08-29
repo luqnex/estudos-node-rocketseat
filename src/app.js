87981aa39e583a649375c5bbc0295e4f680941e9
Lucas Costa <lucas.costa4002@gmail.com>
2022-08-29 20:37:44 -0300

    feat: :sparkles: Adjust in connect from mongo db

--- src/app.js
+++ src/app.js
@@ -1,30 +1,18 @@
 const express = require("express");
 
-const { randomUUID } = require("crypto");
-
 const cors = require("cors");
 
-const fs = require("fs");
+const { client, connectToDatabase } = require("./database");
 
-const { MongoClient } = require("mongodb");
-
-const url =
-  "mongodb+srv://lucas_costa:lucas121216@cluster0.2swgb.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(url);
-
-const dbName = "crud_rocketseat";
+connectToDatabase();
 
 const app = express();
 
-// connect().catch(console.dir);
+const dbName = "crud_rocketseat";
 
 // middleware
 app.use(express.json());
 app.use(cors());
-
-let products = [];
-
-productFile();
 
 app.get("/", (req, res) => {
   res.json({ message: "Oks" });
@@ -34,11 +22,8 @@ app.get("/", (req, res) => {
 app.get("/getByName", (req, res) => {
   const name = req.body.name;
 
-  async function connect() {
+  (async function () {
     try {
-      await client.connect();
-      console.log("Connected correctly to server");
-
       const db = client.db(dbName);
 
       const col = db.collection("product");
@@ -48,20 +33,13 @@ app.get("/getByName", (req, res) => {
       res.json(myDoc);
     } catch (err) {
       console.log(err.stack);
-    } finally {
-      await client.close();
     }
-  }
-
-  connect();
+  })();
 });
 
 app.get("/getAll", (req, res) => {
-  async function connect() {
+  (async function () {
     try {
-      await client.connect();
-      console.log("Connected correctly to server");
-
       const db = client.db(dbName);
 
       const col = db.collection("product");
@@ -71,22 +49,15 @@ app.get("/getAll", (req, res) => {
       res.json(myDoc);
     } catch (err) {
       console.log(err.stack);
-    } finally {
-      await client.close();
     }
-  }
-
-  connect();
+  })();
 });
 
 app.post("/createProduct", (req, res) => {
   const { name, price } = req.body;
 
-  async function connect() {
+  (async function () {
     try {
-      await client.connect();
-      console.log("Connected correctly to server");
-
       const db = client.db(dbName);
 
       const col = db.collection("product");
@@ -96,24 +67,16 @@ app.post("/createProduct", (req, res) => {
       res.json("Product created from DB");
     } catch (err) {
       console.log(err.stack);
-    } finally {
-      await client.close();
     }
-  }
-
-  connect();
+  })();
 });
 
 // TODO: Delete não está funcionando.
 app.delete("/deleteById", (req, res) => {
   const id = req.body.id;
-  console.log(id);
 
-  async function connect() {
+  (async function connect() {
     try {
-      await client.connect();
-      console.log("Connected correctly to server");
-
       const db = client.db(dbName);
 
       const col = db.collection("product");
@@ -123,12 +86,12 @@ app.delete("/deleteById", (req, res) => {
       res.json("Product deleted from DB");
     } catch (err) {
       console.log(err.stack);
-    } finally {
-      await client.close();
     }
-  }
+  })();
+});
 
-  connect();
+app.listen(3001, () => {
+  console.log("rodando na porta 3001");
 });
 
 /**
@@ -136,78 +99,3 @@ app.delete("/deleteById", (req, res) => {
  * Params => /products/:id
  * Query => /products?id=1&value=25
  */
-
-app.post("/products", (req, res) => {
-  const { name, price } = req.body;
-
-  const product = {
-    id: randomUUID(),
-    name,
-    price,
-  };
-
-  products.push(product);
-
-  fs.writeFile("products.json", JSON.stringify(products), (err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("Produto inserido");
-    }
-  });
-
-  return res.json(product);
-});
-
-app.get("/products", (req, res) => {
-  return res.json(products);
-});
-
-app.get("/products/:id", (req, res) => {
-  const { id } = req.params;
-
-  const product = products.find((product) => product.id === id);
-
-  return res.json(product);
-});
-
-app.put("/products/:id", (req, res) => {
-  const { id } = req.params;
-  const { name, price } = req.body;
-
-  const productIndex = products.findIndex((product) => product.id === id);
-
-  products[productIndex] = {
-    ...products[productIndex],
-    name,
-    price,
-  };
-
-  productFile();
-
-  return res.json({ message: "Produto alterado com sucesso!" });
-});
-
-app.delete("/products/:id", (req, res) => {
-  const { id } = req.params;
-
-  const productIndex = products.findIndex((product) => product.id === id);
-
-  products.splice(productIndex, 1);
-
-  return res.json({ message: "Produto removido com sucesso!" });
-});
-
-function productFile() {
-  fs.readFile("products.json", "utf-8", (err, data) => {
-    if (err) {
-      console.log(err);
-    } else {
-      products = JSON.parse(data);
-    }
-  });
-}
-
-app.listen(3001, () => {
-  console.log("rodando na porta 3001");
-});
